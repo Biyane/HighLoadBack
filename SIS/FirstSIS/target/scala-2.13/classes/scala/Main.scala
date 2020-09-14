4587e62d091c121a19d1b93382074d64c4b1d115
Biyane <47244644+Biyane@users.noreply.github.com>
2020-09-14 14:57:52 +0600

    first

---
+++ SIS/FirstSIS/target/scala-2.13/classes/scala/Main.scala
@@ -0,0 +1,23 @@
+class Main(var numbers:Array[Double], var operators: String) {
+  var res: Double = 0
+  var temp:Int = 0
+  val indexedSeq = for(i <- 0 to 10 if i % 2 == 0) yield i
+  for(i <- indexedSeq; j <- 0 to operators.length){
+
+  }
+  operators(0) match {
+    case '+' => res = numbers(0) + numbers(1)
+    case '-' => res = numbers(0) - numbers(1)
+    case '*' => res = numbers(0) * numbers(1)
+    case '/' => res = numbers(0) / numbers(1)
+  }
+  for(i <- 1 to operators.length - 1; j <- 2 to numbers.length - 1){
+      operators(i) match {
+        case '+' => res += numbers(j)
+        case '*' => res *= numbers(j)
+        case '-' => res -= numbers(j)
+        case '/' => res /= numbers(j)
+      }
+  }
+  def printNum() = print(res)
+}
